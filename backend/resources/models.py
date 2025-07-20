dfbe69b0fd058a7f405f2421d458c75649644fb7
gathanuarobert <robertgathanua@gmail.com>
2025-07-20 18:30:56 +0300

    Created ResourceCategory model with unique name and optional description and aded str method for readable representation.

--- backend/resources/models.py
+++ backend/resources/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class ResourceCategory(models.Model):
+    name = models.CharField(max_length=150, unique=True)
+    description = models.TextField(blank=True, default="", help_text='Optional description for categories')
+
+    def __str__(self):
+        return self.name   
