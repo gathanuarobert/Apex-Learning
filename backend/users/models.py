a0d2ab314e8fb8cebee777ad591900f4d2cb6e00
gathanuarobert <robertgathanua@gmail.com>
2025-07-21 10:59:50 +0300

    Implement custom user model and role-based profiles.

--- backend/users/models.py
+++ backend/users/models.py
@@ -1,3 +1,96 @@
+from django.contrib.auth.models import AbstractBaseUser, UserManager, PermissionsMixin
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+class CustomUserManager(UserManager):
+    def _create_user(self, email, password, **extra_fields):
+        if not email:
+            raise ValueError('Email field must be set')
+        
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+
+        return user
+    
+    def create_user(self,email=None, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', False)
+        extra_fields.setdefault('is_superuser', False)
+        return self._create_user(email, password, **extra_fields)
+    
+    def create_superuser(self, email=None, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        return self._create_user(email, password, **extra_fields)
+
+class User(AbstractBaseUser, PermissionsMixin):
+    ROLE_CHOICES = (
+        ('student', 'Student'),
+        ('teacher', 'Teacher'),
+        ('parent', 'Parent'),
+    )
+    email = models.EmailField(unique=True)
+    name = models.CharField(max_length=30, blank=True, default='')
+    role = models.CharField(max_length=10, choices=ROLE_CHOICES, blank=True, null=True)
+
+    is_active = models.BooleanField(default=True)
+    is_superuser = models.BooleanField(default=False)
+    is_staff = models.BooleanField(default=False)
+
+    date_joined = models.DateTimeField(default=timezone.now)
+    last_login = models.DateTimeField(null=True, blank=True)
+
+    objects = CustomUserManager()
+
+    USERNAME_FIELD = 'email'
+    EMAIL_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    class Meta:
+        verbose_name = 'User'
+        verbose_name_plural = 'Users'
+
+    def get_full_name(self):
+        return self.name
+
+    def get_short_name(self):
+        return self.name or self.email.split('@')[0]    
+    
+class TeacherProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='teacher_profile', null=True, blank=True)
+    subject = models.CharField(max_length=100, blank=True, default='')
+
+    def __str__(self):
+        return f'Teacher Profile of {self.user.get_full_name()}' if self.user else 'Teacher Profile'
+
+    def save(self, *args, **kwargs):
+        if self.user.role != 'teacher':
+            raise ValueError("Assigned user is not a teacher")
+        super().save(*args, **kwargs)
+
+
+class StudentProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='student_profile', null=True, blank=True)
+    grade = models.CharField(max_length=10, blank=True, default='') 
+
+    def __str__(self):
+        return f'Student Profile of {self.user.get_full_name()}' if self.user else 'Student Profile'
+    
+    def save(self, *args, **kwargs):
+        if self.user.role != 'student':
+            raise ValueError("Assigned user is not a student")
+        super().save(*args, **kwargs)
+
+class ParentProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='parent_profile', null=True, blank=True)
+    children = models.ManyToManyField(StudentProfile, blank=True, related_name='parents')
+
+    def __str__(self):
+        return f'Parent Profile of {self.user.get_full_name()}' if self.user else 'Parent Profile'
+
+    def save(self, *args, **kwargs):
+        if self.user.role != 'parent':
+            raise ValueError("Assigned user is not a parent")
+        super().save(*args, **kwargs)
