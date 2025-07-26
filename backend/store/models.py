7b117525d80aff54aaa4255055836b93d9a38e36
gathanuarobert <robertgathanua@gmail.com>
2025-07-26 09:12:49 +0300

    Create store models.

--- backend/store/models.py
+++ backend/store/models.py
@@ -1,9 +1,13 @@
 from django.db import models
+from users.models import User
 
 class Product(models.Model):
-    # fields here
-    pass
+    product_id = models.PositiveIntegerField()
+    name = models.CharField(max_length=255)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
 
 class Order(models.Model):
-    # fields here
-    pass
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField()
+    order_date = models.DateTimeField(auto_now_add=True)
