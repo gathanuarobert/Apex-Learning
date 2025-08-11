4698fb71f32d97abcd5e94f0e60481a9ffe5a1a7
gathanuarobert <robertgathanua@gmail.com>
2025-08-11 09:26:27 +0300

    Add pricing field in the models.

--- backend/resources/models.py
+++ backend/resources/models.py
@@ -1,15 +1,20 @@
 from django.db import models
 import uuid
 
+
 class Subject(models.Model):
     name = models.CharField(max_length=100, unique=True)
+
     def __str__(self):
         return self.name
 
+
 class Grade(models.Model):
     name = models.CharField(max_length=100, unique=True)
+
     def __str__(self):
         return self.name
+
 
 class Note(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False, unique=True)
@@ -18,10 +23,13 @@ class Note(models.Model):
     grade = models.ForeignKey(Grade, on_delete=models.CASCADE, null=True, blank=True)
     content = models.TextField()
     file = models.FileField(upload_to='notes_files/', blank=True, null=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
     def __str__(self):
         return f"{self.title} ({self.subject} - {self.grade})"
+
 
 class PastPaper(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False, unique=True)
@@ -30,9 +38,12 @@ class PastPaper(models.Model):
     grade = models.ForeignKey(Grade, on_delete=models.CASCADE, null=True, blank=True)
     year = models.PositiveIntegerField()
     file = models.FileField(upload_to='pastpapers_files/', blank=True, null=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)
     created_at = models.DateTimeField(auto_now_add=True)
+
     def __str__(self):
         return f"{self.subject} - {self.grade} - {self.year}"
+
 
 class Exam(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False, unique=True)
@@ -42,15 +53,19 @@ class Exam(models.Model):
     date = models.DateField()
     description = models.TextField(blank=True, default="")
     file = models.FileField(upload_to='exams_files/', blank=True, null=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)
     created_at = models.DateTimeField(auto_now_add=True)
+
     def __str__(self):
         return f"{self.title} ({self.subject} - {self.grade})"
+
 
 class News(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False, unique=True)
     headline = models.CharField(max_length=150)
     body = models.TextField()
-    file = models.FileField(upload_to='news_files/', blank=True,null=True)
+    file = models.FileField(upload_to='news_files/', blank=True, null=True)
     published_at = models.DateTimeField(auto_now_add=True)
+
     def __str__(self):
         return self.headline
