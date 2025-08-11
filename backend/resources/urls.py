65946748920e80972cbb7c75e880209885d4d11c
gathanuarobert <robertgathanua@gmail.com>
2025-08-11 12:18:55 +0300

    Refactor the urls.py file.

--- backend/resources/urls.py
+++ backend/resources/urls.py
@@ -1,6 +1,5 @@
 from django.conf import settings
 from django.conf.urls.static import static
-
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 from .views import NoteViewSet, PastPaperViewSet, ExamViewSet, NewsViewSet
@@ -12,7 +11,7 @@ router.register(r'exams', ExamViewSet, basename='exam')
 router.register(r'news', NewsViewSet, basename='news')
 
 urlpatterns = [
-    path('',include(router.urls)),    
+    path('', include(router.urls)),
 ]
 
 if settings.DEBUG:
