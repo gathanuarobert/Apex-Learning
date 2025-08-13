4dc4e820e2d768192b85990af521c9bef2e01f2e
gathanuarobert <robertgathanua@gmail.com>
2025-08-13 18:43:14 +0300

    Add the Wallet and Transaction models in admin.py file.

--- backend/payments/admin.py
+++ backend/payments/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from .models import Payment
+from .models import Payment, Wallet, Transaction
 
 # Register your models here.
 admin.site.register(Payment)
+admin.site.register(Wallet)
+admin.site.register(Transaction)
