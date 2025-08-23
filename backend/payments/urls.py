7ba5b6ee630ee6301b4bfe15ffaeea8d14f4c1e5
gathanuarobert <robertgathanua@gmail.com>
2025-08-23 18:30:27 +0300

    Edit imports to ensure views are imported properly to avoid errors.

--- backend/payments/urls.py
+++ backend/payments/urls.py
@@ -1,9 +1,14 @@
 from django.urls import path
-from .views import WalletDepositInitiateView, OneTimePurchaseInitiateView, mpesa_callback, WalletPurchaseAPIView
+from .views import (
+    WalletDepositInitiateView,
+    OneTimePurchaseInitiateView,
+    WalletPurchaseAPIView,
+    mpesa_callback_view,
+)
 
 urlpatterns = [
-    path('wallet/deposit/initiate/', WalletDepositInitiateView.as_view(), name='wallet-deposit-initiate'),
-    path('purchase/resource/initiate/', OneTimePurchaseInitiateView.as_view(), name='one-time-purchase-initiate'),
-    path('wallet/purchase/', WalletPurchaseAPIView.as_view(), name='wallet-purchase'),
-    path('mpesa-callback/', mpesa_callback, name='mpesa-callback'),
+    path("wallet/deposit/initiate/", WalletDepositInitiateView.as_view(), name="wallet-deposit-initiate"),
+    path("purchase/resource/initiate/", OneTimePurchaseInitiateView.as_view(), name="one-time-purchase-initiate"),
+    path("wallet/purchase/", WalletPurchaseAPIView.as_view(), name="wallet-purchase"),
+    path("mpesa-callback/", mpesa_callback_view, name="mpesa-callback"),
 ]
