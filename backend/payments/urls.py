8297e6511f495e2a27822fc9c9d69f8e0fb5693f
gathanuarobert <robertgathanua@gmail.com>
2026-02-16 22:55:54 +0300

    Add new urls for new views.

--- backend/payments/urls.py
+++ backend/payments/urls.py
@@ -6,12 +6,14 @@ from .views import (
     mpesa_callback_view,
     TransactionHistoryView,
     AdminTransactionHistoryView,
+    WalletDetailView,  # Add this import
 )
 
 urlpatterns = [
     path("wallet/deposit/initiate/", WalletDepositInitiateView.as_view(), name="wallet-deposit-initiate"),
     path("purchase/resource/initiate/", OneTimePurchaseInitiateView.as_view(), name="one-time-purchase-initiate"),
-    path("wallet/purchase/", WalletPurchaseAPIView.as_view(), name="wallet-purchase"),
+    path("wallet/purchase/", WalletPurchaseAPIView.as_view(), name="wallet-purchase"),  # Changed this line
+    path("wallet/", WalletDetailView.as_view(), name="wallet-detail"),  # Changed this line
     path("mpesa-callback/", mpesa_callback_view, name="mpesa-callback"),
     path("transactions/", TransactionHistoryView.as_view(), name="transaction-history"),
     path("admin/transactions/", AdminTransactionHistoryView.as_view(), name="admin-transactions"),
