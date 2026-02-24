bdce3b4944b6e50cf8f2ff6f27c0b4af58c6a696
gathanuarobert <robertgathanua@gmail.com>
2026-02-24 21:00:20 +0300

    Incorporate the loader into the web app.

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import { GoogleOAuthProvider } from "@react-oauth/google";
 import App from "./App";
+import Loader from "./components/Loader";
 import "./index.css";
 
 const clientId = "702572481682-gi5q72056ucms8ciifg5vlbjgvni880h.apps.googleusercontent.com";
@@ -11,7 +12,9 @@ ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <GoogleOAuthProvider clientId={clientId}>
       <BrowserRouter>
-        <App />
+        <Loader>
+          <App />
+        </Loader>
       </BrowserRouter>
     </GoogleOAuthProvider>
   </React.StrictMode>
