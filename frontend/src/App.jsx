bd9ffefd72e7c84ef14b8bb1748bd6df48f731cd
gathanuarobert <robertgathanua@gmail.com>
2026-03-23 15:15:05 +0300

    Add new routes to App.jsx.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -19,46 +19,32 @@ import PastPapersPage from './pages/PastPapersPage'
 import ForgotPassword from './pages/ForgotPaasword'
 import RevisionPage from './pages/RevisionPage'
 import NewsPage from './pages/NewsPage'
-// const useAuthCheck = () => {
-//   useEffect(() => {
-//     const checkToken = async () => {
-//       const token = localStorage.getItem('token')
-//       if (!token && window.location.pathname !== '/login') {
-//         window.location.href = '/login'
-//         // Validate token
-//       }
-//     }
-//     checkToken()
-//   }, [])
-// }
+import PaymentComplete from './pages/PaymentComplete'
 
 function App() {
-  // useAuthCheck()
   return (
-   
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<AboutUs />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/add-product" element={<AddProduct />} />
-        <Route path='/edit-product/:id' element={<EditProduct />} />
-        <Route path='/login' element={<Login />} />
-        <Route path='/news-composer' element={<NewsComposer />} />
-        <Route path='/account' element={<AccountPage />} />
-        <Route path='/admin' element={<AdminUserPage />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/admin-news" element={<AdminNews />} />
-        <Route path="/contact" element={<ContactUs />} />
-        <Route path="/notes" element={<NotesPage />} />
-        <Route path="/user-dashboard" element={<UserDashboard />} />
-        <Route path="/exams" element={<ExamsPage />} />
-        <Route path="/past-papers" element={<PastPapersPage />} />
-        <Route path="/forgot-password" element={<ForgotPassword />} />
-        <Route path="/revision" element={<RevisionPage />} />
-        <Route path="/news" element={<NewsPage />} />
-        {/* Add more routes as needed */}
-      </Routes>
-  
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/about" element={<AboutUs />} />
+      <Route path="/register" element={<Register />} />
+      <Route path="/add-product" element={<AddProduct />} />
+      <Route path='/edit-product/:id' element={<EditProduct />} />
+      <Route path='/login' element={<Login />} />
+      <Route path='/news-composer' element={<NewsComposer />} />
+      <Route path='/account' element={<AccountPage />} />
+      <Route path='/admin' element={<AdminUserPage />} />
+      <Route path="/dashboard" element={<Dashboard />} />
+      <Route path="/admin-news" element={<AdminNews />} />
+      <Route path="/contact" element={<ContactUs />} />
+      <Route path="/notes" element={<NotesPage />} />
+      <Route path="/user-dashboard" element={<UserDashboard />} />
+      <Route path="/exams" element={<ExamsPage />} />
+      <Route path="/past-papers" element={<PastPapersPage />} />
+      <Route path="/forgot-password" element={<ForgotPassword />} />
+      <Route path="/revision" element={<RevisionPage />} />
+      <Route path="/news" element={<NewsPage />} />
+      <Route path="/payment/complete" element={<PaymentComplete />} />
+    </Routes>
   )
 }
 
