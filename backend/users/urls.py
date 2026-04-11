1b268199db80a8d6edcabdd1f3b2dfa97fd0f8bf
gathanuarobert <robertgathanua@gmail.com>
2026-04-11 21:28:43 +0300

    Add new urls to match new views created.

--- backend/users/urls.py
+++ backend/users/urls.py
@@ -1,6 +1,8 @@
 # users/urls.py
 from django.urls import path
-from .views import RegistrationUserView, LoginView, AddChildrenToParentView, UserListView, UserDeleteView, CurrentUserView,GoogleLoginView
+from .views import (RegistrationUserView, LoginView, AddChildrenToParentView, 
+                    UserListView, UserDeleteView, CurrentUserView,GoogleLoginView,
+                    UpdateUserView, ChangePasswordView)
 
 urlpatterns = [
     path('register/', RegistrationUserView.as_view(), name='register'),
@@ -10,4 +12,6 @@ urlpatterns = [
     path('', UserListView.as_view(), name='user-list'),  # now just /api/users/
     path('<int:pk>/', UserDeleteView.as_view(), name='user-delete'),  # now /api/users/1/
     path('google-login/', GoogleLoginView.as_view(), name='google-login'),
+    path('me/update/', UpdateUserView.as_view(), name='update-user'),
+    path('me/change-password/', ChangePasswordView.as_view(), name='change-password'),
 ]
