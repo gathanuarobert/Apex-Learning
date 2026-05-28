d02bde11acf56c8a9c2e37ab8da3d54d8a51f2c3
gathanuarobert <robertgathanua@gmail.com>
2026-05-28 20:37:46 +0300

    Create new urls for the newly created views.

--- backend/users/urls.py
+++ backend/users/urls.py
@@ -1,19 +1,22 @@
-# users/urls.py
 from django.urls import path
-from .views import (RegistrationUserView, LoginView, AddChildrenToParentView, 
-                    UserListView, UserDeleteView, CurrentUserView,GoogleLoginView,
-                    UpdateUserView, ChangePasswordView, GuestStatusView,
-                    )
+from .views import (
+    RegistrationUserView, LoginView, AddChildrenToParentView,
+    UserListView, UserDeleteView, CurrentUserView, GoogleLoginView,
+    UpdateUserView, ChangePasswordView, GuestStatusView,
+    ForgotPasswordView, ResetPasswordView,
+)
 
 urlpatterns = [
-    path('register/', RegistrationUserView.as_view(), name='register'),
-    path('login/', LoginView.as_view(), name='login'),
-    path('add-children/', AddChildrenToParentView.as_view(), name='add-children'),
-    path('me/', CurrentUserView.as_view(), name='current-user'),
-    path('', UserListView.as_view(), name='user-list'),  # now just /api/users/
-    path('<int:pk>/', UserDeleteView.as_view(), name='user-delete'),  # now /api/users/1/
-    path('google-login/', GoogleLoginView.as_view(), name='google-login'),
-    path('me/update/', UpdateUserView.as_view(), name='update-user'),
-    path('me/change-password/', ChangePasswordView.as_view(), name='change-password'),
-    path('guest-status/', GuestStatusView.as_view(), name='guest-status'),
+    path('register/',           RegistrationUserView.as_view(),   name='register'),
+    path('login/',              LoginView.as_view(),               name='login'),
+    path('add-children/',       AddChildrenToParentView.as_view(), name='add-children'),
+    path('me/',                 CurrentUserView.as_view(),         name='current-user'),
+    path('me/update/',          UpdateUserView.as_view(),          name='update-user'),
+    path('me/change-password/', ChangePasswordView.as_view(),      name='change-password'),
+    path('google-login/',       GoogleLoginView.as_view(),         name='google-login'),
+    path('guest-status/',       GuestStatusView.as_view(),         name='guest-status'),
+    path('forgot-password/',    ForgotPasswordView.as_view(),      name='forgot-password'),
+    path('reset-password/',     ResetPasswordView.as_view(),       name='reset-password'),
+    path('',                    UserListView.as_view(),            name='user-list'),
+    path('<int:pk>/',           UserDeleteView.as_view(),          name='user-delete'),
 ]
